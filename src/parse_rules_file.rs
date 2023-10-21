73b20397451f399e54f5a167089fc5d63b9a9d9b
Max Heinritz <max@loop.com>
2023-10-21 11:42:58 -0500

    add disallowed rules

--- src/parse_rules_file.rs
+++ src/parse_rules_file.rs
@@ -7,7 +7,39 @@ use std::io::Read;
 
 #[derive(Serialize, Deserialize)]
 pub struct Rules {
-    allow: HashMap<String, Vec<String>>,
+    pub allow: HashMap<String, Vec<String>>,
+}
+
+impl Rules {
+    pub fn extract_unique_names(&self) -> Vec<String> {
+        let mut unique_names = self.allow.keys().cloned().collect::<Vec<_>>();
+
+        for names in self.allow.values() {
+            for name in names {
+                unique_names.push(name.clone());
+            }
+        }
+
+        unique_names.sort();
+        unique_names.dedup();
+
+        unique_names
+    }
+
+    pub fn get_allow_rules(&self, key: &str) -> Option<&Vec<String>> {
+        self.allow.get(key)
+    }
+
+    pub fn get_disallowed_rules(&self, key: &str) -> Option<Vec<String>> {
+        let unique_names = self.extract_unique_names();
+        let allow_rules = self.get_allow_rules(key)?;
+        let diff = find_difference(unique_names, allow_rules.clone());
+        Some(diff)
+    }
+}
+
+fn find_difference(a: Vec<String>, b: Vec<String>) -> Vec<String> {
+    a.into_iter().filter(|x| !b.contains(x)).collect()
 }
 
 pub fn parse_rules_file(file_path: &str) -> Result<Rules, Box<dyn Error>> {
--- src/visit_directory.rs
+++ src/visit_directory.rs
@@ -2,10 +2,10 @@ use std::error::Error;
 
 use serde_json::to_string_pretty;
 
-use crate::{list_files_and_directories::list_files_and_directories, parse_rules_file};
+use crate::{list_files_and_directories, parse_rules_file};
 
 pub fn visit_directory(path: &str) -> Result<(), Box<dyn Error>> {
-    let map = list_files_and_directories(path)?;
+    let map = list_files_and_directories::list_files_and_directories(path)?;
     let directories = map.get("directories").unwrap();
     let files = map.get("files").unwrap();
 
@@ -22,9 +22,25 @@ pub fn visit_directory(path: &str) -> Result<(), Box<dyn Error>> {
     let rules_path = format!("{}/.deplint.rules.json", path);
     let rules = parse_rules_file::parse_rules_file(&rules_path)?;
     // Print the body of the config file.
-    println!("Rules:");
-    let pretty_json = to_string_pretty(&rules).expect("Failed to serialize Rules struct");
-    println!("{}", pretty_json);
+    // println!("Rules:");
+    // let pretty_json = to_string_pretty(&rules).expect("Failed to serialize Rules struct");
+    // println!("{}", pretty_json);
+    let unique_names = rules.extract_unique_names();
+
+    for directory in directories {
+        if !unique_names.contains(&directory) {
+            continue;
+        }
+        let full_path = format!("{}/{}", path, directory);
+        let disallowed_rules = rules.get_disallowed_rules(&directory);
+        println!("Disallowed rules for directory: {}", directory);
+        if let Some(disallowed_rules) = disallowed_rules {
+            for disallowed_rule in disallowed_rules {
+                println!("{}", disallowed_rule);
+            }
+        }
+        // visit_directory(&full_path)?;
+    }
 
     for file in files {
         if !file.ends_with(".ts") {
