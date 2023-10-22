6b5f5e12f9bf298a8fe8e066c26a33c8844125fc
Max Heinritz <max@loop.com>
2023-10-21 21:39:54 -0500

    renames

--- src/visit.rs
+++ src/visit.rs
@@ -5,25 +5,25 @@ use crate::{files, imports, rules};
 pub fn visit_path(
     root_path: &Path,
     disallowed_imports: Vec<String>,
-    path: &Path,
+    target: &Path,
 ) -> Result<(), Box<dyn Error>> {
     // println!("Visiting directory: {}", path);
     // println!("Disallowed imports: {:?}", disallowed_imports);
-    let map = files::list_files_and_directories(path)?;
+    let map = files::list_files_and_directories(target)?;
     let directories = map.get("directories").unwrap();
     let files = map.get("files").unwrap();
-    let rules_path = path.join(".deplint.rules.json");
+    let rules_path = target.join(".deplint.rules.json");
     let rules_result = rules::read_rules_file(&rules_path);
     let rules = rules_result.ok();
     for directory in directories {
-        let full_path = path.join(directory);
+        let full_path = target.join(directory);
         let mut dir_disallowed_imports = disallowed_imports.clone();
         if let Some(rules) = &rules {
             let disallowed_siblings_result = rules.get_disallowed_siblings(&directory);
             if let Some(disallowed_siblings) = disallowed_siblings_result {
                 let new_disallowed_imports = disallowed_siblings
                     .iter()
-                    .map(|s| format!("{}/{}", path.to_str().expect(""), s))
+                    .map(|s| format!("{}/{}", target.to_str().expect(""), s))
                     .map(|s| s.replace("/Users/maxheinritz/loop-payments/backend/src", "src"))
                     .collect::<Vec<_>>();
                 dir_disallowed_imports.extend(new_disallowed_imports);
@@ -38,7 +38,7 @@ pub fn visit_path(
         if !file.ends_with(".ts") {
             continue;
         }
-        let full_path = path.join(file);
+        let full_path = target.join(file);
         let imports = imports::read_imports_from_file(&full_path)?;
         for import in imports {
             for disallowed_import in &disallowed_imports {
