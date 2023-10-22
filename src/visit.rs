a53abf085476f566a3af602d2bd34d446fcbe215
Max Heinritz <max@loop.com>
2023-10-21 21:44:07 -0500

    helper funs

--- src/visit.rs
+++ src/visit.rs
@@ -1,20 +1,34 @@
+use crate::{
+    files, imports,
+    rules::{self, Rules},
+};
 use std::{error::Error, path::Path};
-
-use crate::{files, imports, rules};
 
 pub fn visit_path(
     root: &Path,
     disallowed_imports: Vec<String>,
     target: &Path,
 ) -> Result<(), Box<dyn Error>> {
-    // println!("Visiting directory: {}", path);
-    // println!("Disallowed imports: {:?}", disallowed_imports);
     let map = files::list_files_and_directories(target)?;
     let directories = map.get("directories").unwrap();
     let files = map.get("files").unwrap();
     let rules_path = target.join(".deplint.rules.json");
     let rules_result = rules::read_rules_file(&rules_path);
     let rules = rules_result.ok();
+
+    visit_directories(root, &disallowed_imports, &rules, &target, &directories)?;
+    check_files_for_disallowed_imports(&disallowed_imports, &target, &files)?;
+
+    Ok(())
+}
+
+fn visit_directories(
+    root: &Path,
+    disallowed_imports: &Vec<String>,
+    rules: &Option<Rules>,
+    target: &Path,
+    directories: &Vec<String>,
+) -> Result<(), Box<dyn Error>> {
     for directory in directories {
         let full_path = target.join(directory);
         let mut dir_disallowed_imports = disallowed_imports.clone();
@@ -31,9 +45,14 @@ pub fn visit_path(
         }
         visit_path(root, dir_disallowed_imports, &full_path)?;
     }
-    if disallowed_imports.is_empty() {
-        return Ok(());
-    }
+    Ok(())
+}
+
+fn check_files_for_disallowed_imports(
+    disallowed_imports: &Vec<String>,
+    target: &Path,
+    files: &Vec<String>,
+) -> Result<(), Box<dyn Error>> {
     for file in files {
         if !file.ends_with(".ts") {
             continue;
@@ -41,7 +60,7 @@ pub fn visit_path(
         let full_path = target.join(file);
         let imports = imports::read_imports_from_file(&full_path)?;
         for import in imports {
-            for disallowed_import in &disallowed_imports {
+            for disallowed_import in disallowed_imports {
                 if import.starts_with(disallowed_import) {
                     println!("{} imports {}", full_path.to_str().expect(""), import);
                 }
