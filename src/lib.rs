fc1e1d29c15ee7664a31fda2b797c9da16b886a8
Max Heinritz <max@loop.com>
2023-10-22 09:42:28 -0500

    renames

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, path::Path};
 
-mod disallowed_imports;
+mod disallowed;
 mod files;
 mod imports;
 mod root;
@@ -14,7 +14,7 @@ pub use violations::Violation;
 pub fn list_violations(target: &Path) -> Result<Vec<violations::Violation>, Box<dyn Error>> {
     let root = root::find_package_json_directory(target)
         .ok_or("No package.json found in any parent directory.")?;
-    let disallowed_imports = disallowed_imports::get_initial_disallowed_imports(&root, target);
+    let disallowed_imports = disallowed::get_initial_disallowed_imports(&root, target);
     let violations = visit::visit_path(root.as_ref(), &disallowed_imports, target)?;
     Ok(violations)
 }
