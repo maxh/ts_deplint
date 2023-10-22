80c82b0ae014d3e55ecdf829221e3a1fb7f2fb73
Max Heinritz <max@loop.com>
2023-10-22 10:00:34 -0500

    lint

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use ts_deplint::{list_violations, pretty_print_violations};
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        eprintln!("Usage: {} <path>", args[0]);
+    if args.len() < 3 {
+        eprintln!("Usage: [lint|diagram] {} <path>", args[0]);
         std::process::exit(1);
     }
 
-    let target = Path::new(&args[1]);
-    let violations = list_violations(target)?;
-    pretty_print_violations(violations);
+    let command = &args[1];
+    let target = Path::new(&args[2]);
+    if (command != "lint" && command != "diagram") || !target.exists() {
+        eprintln!("Usage: [lint|diagram] {} <path>", args[0]);
+        std::process::exit(1);
+    }
+    if command == "lint" {
+        let violations = list_violations(target)?;
+        pretty_print_violations(violations);
+    }
+    if command == "diagram" {}
     Ok(())
 }
