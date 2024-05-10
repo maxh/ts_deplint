8f4ede5a6e428cb0eb906fe41e25fdda4c468b45
Erik <162160879+ens-loop@users.noreply.github.com>
2024-05-09 20:26:49 -0500

    option to highlight circular dependencies (#57)
    Highlighting circular dependencies, suggested in #26
    
    <img width="268" alt="Screenshot 2024-04-30 at 7 10 23 AM"
    src="https://github.com/maxh/ts_deplint/assets/162160879/2932eb44-3660-4510-923a-18795596a810">
    
    Note: The arrow heads are not highlighted because mermaid does not
    currently support setting the color of individual arrow heads.
    
    Other considerations/alternatives:
    - allow customization of color with option
    - for circular dependency, could show single arrow with heads on both
    sides (instead of 2 x 1-way arrows)

--- src/diagram.rs
+++ src/diagram.rs
@@ -1,6 +1,6 @@
 extern crate serde_yaml;
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 use std::error::Error;
 use std::fs;
 use std::io::{self, Write};
@@ -10,24 +10,36 @@ use crate::rules::read_rules_file;
 use crate::RULES_FILE_NAME;
 
 /// Recursively find directories containing a rules file and update the diagram.
-pub fn update_diagrams_recursively(dir: &Path) -> Result<(), Box<dyn Error>> {
+pub fn update_diagrams_recursively(
+    dir: &Path,
+    show_circular_dependencies: bool,
+) -> Result<(), Box<dyn Error>> {
     if dir.join(RULES_FILE_NAME).exists() {
         let readme_path = dir.join("README.md");
-        update_readme_with_diagram(&dir.join(RULES_FILE_NAME), &readme_path)?;
+        update_readme_with_diagram(
+            &dir.join(RULES_FILE_NAME),
+            &readme_path,
+            show_circular_dependencies,
+        )?;
     }
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            update_diagrams_recursively(&path)?;
+            update_diagrams_recursively(&path, show_circular_dependencies)?;
         }
     }
     Ok(())
 }
 
-fn get_allows(yaml_path: &Path) -> Result<BTreeMap<String, Vec<String>>, Box<dyn Error>> {
+fn get_allows(yaml_path: &Path) -> Result<BTreeMap<String, BTreeSet<String>>, Box<dyn Error>> {
     let yaml_rules = read_rules_file(yaml_path)?;
-    return Ok(yaml_rules.allow);
+    let converted_rules = yaml_rules
+        .allow
+        .into_iter()
+        .map(|(source, targets)| (source, BTreeSet::from_iter(targets.into_iter())))
+        .collect::<BTreeMap<String, BTreeSet<String>>>();
+    Ok(converted_rules)
 }
 
 fn get_other_readme_lines(readme_path: &Path) -> io::Result<(Vec<String>, Vec<String>)> {
@@ -62,6 +74,7 @@ fn get_other_readme_lines(readme_path: &Path) -> io::Result<(Vec<String>, Vec<St
 pub fn update_readme_with_diagram(
     yaml_path: &Path,
     readme_path: &Path,
+    show_circular_dependencies: bool,
 ) -> Result<(), Box<dyn Error>> {
     let allows = get_allows(yaml_path)?;
 
@@ -71,16 +84,23 @@ pub fn update_readme_with_diagram(
 
     let (before_dep_diagram_block, after_dep_diagram_block) = get_other_readme_lines(readme_path)?;
 
-    let mut mermaid_edges = allows
-        .iter()
-        .flat_map(|(source, targets)| {
-            targets
-                .iter()
-                .filter(|&target| target != "-")
-                .map(move |target| format!("  {} --> {}", source, target))
-        })
-        .collect::<Vec<String>>();
-    mermaid_edges.sort();
+    let mut circular_edge_indices = vec![];
+    let mut mermaid_edges = vec![];
+    for (source, targets) in &allows {
+        for target in targets {
+            if target == "-" {
+                continue;
+            }
+            let is_circular_dependency = allows
+                .get(target)
+                .map(|deps| deps.contains(source))
+                .unwrap_or(false);
+            if is_circular_dependency {
+                circular_edge_indices.push(format!("{}", mermaid_edges.len()));
+            }
+            mermaid_edges.push(format!("  {} --> {}", source, target));
+        }
+    }
 
     let mut output_lines = Vec::new();
     output_lines.extend(before_dep_diagram_block);
@@ -90,6 +110,12 @@ pub fn update_readme_with_diagram(
     output_lines.push("  subgraph \" \"".to_string());
     output_lines.extend(mermaid_edges);
     output_lines.push("  end".to_string());
+    if show_circular_dependencies && !circular_edge_indices.is_empty() {
+        output_lines.push(format!(
+            "linkStyle {} stroke:red;",
+            circular_edge_indices.join(",")
+        ));
+    }
     output_lines.push("```".to_string());
     output_lines.extend(after_dep_diagram_block);
 
